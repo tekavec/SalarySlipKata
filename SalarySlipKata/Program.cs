1facb92cf777ccb6bcc1b254f2f3c7838a979f49
tekavec <tomaz@codurance.com>
2017-02-23 08:10:58 +0000

    Add project files.

---
+++ SalarySlipKata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalarySlipKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
